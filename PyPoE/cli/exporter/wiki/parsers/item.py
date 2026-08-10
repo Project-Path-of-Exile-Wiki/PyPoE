9c0dc7b9368d07c74dabaa621322e39b533d7b1e
Andrew Beaumont <acbeaumo@gmail.com>
2026-08-10 06:50:14 -0400

    oops

--- PyPoE/cli/exporter/wiki/parsers/item.py
+++ PyPoE/cli/exporter/wiki/parsers/item.py
@@ -2726,6 +2726,8 @@ class ItemsParser(parser.BaseParser):
                     "template": "heist_client",
                     "condition": lambda v: v,
                 },
+            ),
+            (
                 "ValueMulti",
                 {
                     "template": "heist_target_value_multi",
